a91f2ed9d4d6f0bbafb944785eba4b14fb969817
Eric Beyer <ebeyer@scopely.com>
2018-01-11 19:00:09 -0800

    Move card model creation into the service

--- cardApi.service.js
+++ cardApi.service.js
@@ -7,8 +7,28 @@ angular.module('cardApp')
 				
 				var url = 'https://spreadsheets.google.com/feeds/list/1WvRrQUBRSZS6teOcbnCjAqDr-ubUNIxgiVwWGDcsZYM/od6/public/values?alt=json';
 				$http.get(url).then(function (response) {
-					deferred.resolve(response);
-					console.log('ability', response);
+					var abilities = response.data.feed.entry.map(function(item) {
+						return {
+							comment: item['gsx$comment']['$t'],
+							name: item['gsx$name']['$t'],
+							class: item['gsx$class']['$t'],
+							typeicon: item['gsx$typeicon']['$t'],
+							target: item['gsx$target']['$t'],
+							risk: item['gsx$risk']['$t'],
+							hit: item['gsx$hit']['$t'],
+							crithit: item['gsx$crithit']['$t'],
+							miss: item['gsx$miss']['$t'],
+							critmiss: item['gsx$critmiss']['$t'],
+							abilitytext: item['gsx$abilitytext']['$t'],
+							flavortext: item['gsx$flavortext']['$t'],
+							bonus: item['gsx$bonus']['$t'],
+							detriment: item['gsx$detriment']['$t']
+						}
+					}).filter(function(item) {
+						return typeof item.comment === "undefined" || item.comment === '';
+					});
+					deferred.resolve(abilities);
+					console.log('abilities', abilities);
 				}, function (reason) {
 					deferred.reject(reason);
 				});
@@ -20,8 +40,22 @@ angular.module('cardApp')
 				
 				var url = 'https://spreadsheets.google.com/feeds/list/1WvRrQUBRSZS6teOcbnCjAqDr-ubUNIxgiVwWGDcsZYM/o98nayp/public/values?alt=json';
 				$http.get(url).then(function (response) {
-					deferred.resolve(response);
-					console.log('adventurer',response);
+					var adventurers = response.data.feed.entry.map(function(item) {
+						return {
+							comment: item['gsx$comment']['$t'],
+							name: item['gsx$name']['$t'],
+							flavortext: item['gsx$flavortext']['$t'],
+							melee: item['gsx$melee']['$t'],
+							music: item['gsx$music']['$t'],
+							ranged: item['gsx$ranged']['$t'],
+							magic: item['gsx$magic']['$t'],
+							startingabilities: item['gsx$startingabilities']['$t']
+						};
+					}).filter(function(item) {
+						return typeof item.comment === "undefined" || item.comment === '';
+					});
+					deferred.resolve(adventurers);
+					console.log('adventurer',adventurers);
 				}, function (reason) {
 					deferred.reject(reason);
 				});
@@ -33,8 +67,23 @@ angular.module('cardApp')
 				
 				var url = 'https://spreadsheets.google.com/feeds/list/1WvRrQUBRSZS6teOcbnCjAqDr-ubUNIxgiVwWGDcsZYM/oppzxhl/public/values?alt=json';
 				$http.get(url).then(function (response) {
-					deferred.resolve(response);
-					console.log('encounter',response);
+					var encounters = response.data.feed.entry.map(function(item) {
+						return {
+							comment: item['gsx$comment']['$t'],
+							name: item['gsx$name']['$t'],
+							class: item['gsx$class']['$t'],
+							tier: item['gsx$tier']['$t'],
+							health: item['gsx$health']['$t'],
+							ongoing1: item['gsx$ongoing1']['$t'],
+							ongoing2: item['gsx$ongoing2']['$t'],
+							surge: item['gsx$surge']['$t'],
+							flavortext: item['gsx$flavortext']['$t']
+						};
+					}).filter(function(item) {
+						return typeof item.comment === "undefined" || item.comment === '';
+					});
+					deferred.resolve(encounters);
+					console.log('encounters', encounters);
 				}, function (reason) {
 					deferred.reject(reason);
 				});
@@ -46,8 +95,23 @@ angular.module('cardApp')
 				
 				var url = 'https://spreadsheets.google.com/feeds/list/1WvRrQUBRSZS6teOcbnCjAqDr-ubUNIxgiVwWGDcsZYM/oozgoki/public/values?alt=json';
 				$http.get(url).then(function (response) {
-					deferred.resolve(response);
-					console.log('loot',response);
+					var loot = response.data.feed.entry.map(function(item) {
+						return {
+							comment: item['gsx$comment']['$t'],
+							name: item['gsx$name']['$t'],
+							tier: item['gsx$tier']['$t'],
+							numberuses: item['gsx$numberuses']['$t'],
+							usewhen: item['gsx$usewhen']['$t'],
+							tracker: item['gsx$tracker']['$t'],
+							text: item['gsx$text']['$t'],
+							roll: item['gsx$roll']['$t'],
+							flavortext: item['gsx$flavortext']['$t']
+						};
+					}).filter(function(item) {
+						return typeof item.comment === "undefined" || item.comment === '';
+					});
+					deferred.resolve(loot);
+					console.log('loot',loot);
 				}, function (reason) {
 					deferred.reject(reason);
 				});
--- cards.controller.js
+++ cards.controller.js
@@ -6,81 +6,20 @@ angular.module('cardApp', [])
 		
 		function load() {
 			cardApi.getAbilities().then(function(response) {
-				vm.abilities = response.data.feed.entry.map(function(item) {
-					return {
-						comment: item['gsx$comment']['$t'],
-						name: item['gsx$name']['$t'],
-						class: item['gsx$class']['$t'],
-						typeicon: item['gsx$typeicon']['$t'],
-						target: item['gsx$target']['$t'],
-						risk: item['gsx$risk']['$t'],
-						hit: item['gsx$hit']['$t'],
-						crithit: item['gsx$crithit']['$t'],
-						miss: item['gsx$miss']['$t'],
-						critmiss: item['gsx$critmiss']['$t'],
-						abilitytext: item['gsx$abilitytext']['$t'],
-						flavortext: item['gsx$flavortext']['$t'],
-						bonus: item['gsx$bonus']['$t'],
-						detriment: item['gsx$detriment']['$t']
-					}
-				}).filter(function(item) {
-					return typeof item.comment === "undefined" || item.comment === '';
-				});
-				
+				vm.abilities = response;
 				// TODO - eventually parse data into a dictionary... abilities['Magic'] = []; abilities['Music'] = []; etc...
 			});
 			
 			cardApi.getEncounters().then(function(response) {
-				vm.encounters = response.data.feed.entry.map(function(item) {
-					return {
-						comment: item['gsx$comment']['$t'],
-						name: item['gsx$name']['$t'],
-						class: item['gsx$class']['$t'],
-						tier: item['gsx$tier']['$t'],
-						health: item['gsx$health']['$t'],
-						ongoing1: item['gsx$ongoing1']['$t'],
-						ongoing2: item['gsx$ongoing2']['$t'],
-						surge: item['gsx$surge']['$t'],
-						flavortext: item['gsx$flavortext']['$t']
-					};
-				}).filter(function(item) {
-					return typeof item.comment === "undefined" || item.comment === '';
-				});
+				vm.encounters = response;
 			});
 			
 			cardApi.getAdventurers().then(function(response) {
-				vm.adventurers = response.data.feed.entry.map(function(item) {
-					return {
-						comment: item['gsx$comment']['$t'],
-						name: item['gsx$name']['$t'],
-						flavortext: item['gsx$flavortext']['$t'],
-						melee: item['gsx$melee']['$t'],
-						music: item['gsx$music']['$t'],
-						ranged: item['gsx$ranged']['$t'],
-						magic: item['gsx$magic']['$t'],
-						startingabilities: item['gsx$startingabilities']['$t']
-					};
-				}).filter(function(item) {
-					return typeof item.comment === "undefined" || item.comment === '';
-				});
+				vm.adventurers = response;
 			});
 			
 			cardApi.getLoot().then(function(response) {
-				vm.loot = response.data.feed.entry.map(function(item) {
-					return {
-						comment: item['gsx$comment']['$t'],
-						name: item['gsx$name']['$t'],
-						tier: item['gsx$tier']['$t'],
-						numberuses: item['gsx$numberuses']['$t'],
-						usewhen: item['gsx$usewhen']['$t'],
-						tracker: item['gsx$tracker']['$t'],
-						text: item['gsx$text']['$t'],
-						roll: item['gsx$roll']['$t'],
-						flavortext: item['gsx$flavortext']['$t']
-					};
-				}).filter(function(item) {
-					return typeof item.comment === "undefined" || item.comment === '';
-				});
+				vm.loot = response;
 			});
 		}
 	});
